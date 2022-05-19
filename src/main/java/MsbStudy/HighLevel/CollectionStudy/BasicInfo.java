507c67e8c8a4ab3ac002ff98ec31abf14acf1896
wangaijun <2448212706@qq.com>
2022-05-19 09:46:46 +0800

    复习异常处理了

---
+++ src/main/java/MsbStudy/HighLevel/CollectionStudy/BasicInfo.java
@@ -0,0 +1,16 @@
+package MsbStudy.HighLevel.CollectionStudy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 数据结构：所有数据结构分为逻辑结构和物理结构
+ * 逻辑结构：线性表（数组，链表），图，树，栈，队列
+ * 物理结构是逻辑结构的基石：紧密结构，跳转结构
+ * */
+public class BasicInfo {
+    public static void main(String[] args) {
+        List<String> a=new ArrayList<>();
+        a.add("12");
+    }
+}
