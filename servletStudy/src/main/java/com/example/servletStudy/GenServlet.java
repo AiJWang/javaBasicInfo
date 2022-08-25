947719a2ae3cb4b6458ecff2c420e4d328d24829
wangaijun <wangaijun@58.com>
2022-08-25 22:00:33 +0800

    前端

---
+++ servletStudy/src/main/java/com/example/servletStudy/GenServlet.java
@@ -0,0 +1,18 @@
+package com.example.servletStudy;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+/*
+* GenericServlet: 一个抽象类
+* 只需要重写service方法就行，跟实现Servlet一样
+* 与协议无关，可以不是http
+* */
+public class GenServlet extends GenericServlet {
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("GenServlet");
+    }
+}
---
+++ servletStudy/src/main/java/com/example/servletStudy/HttpServletStudy.java
@@ -0,0 +1,14 @@
+package com.example.servletStudy;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+/*
+* 这个servlet是针对http请求的，之前的不是只只对http请求的
+* */
+public class HttpServletStudy extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp){
+        System.out.println("这是个get请求");
+    }
+}
